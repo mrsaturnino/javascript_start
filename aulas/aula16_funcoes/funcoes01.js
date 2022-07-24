d6447823b7543eaa6f252e16e64c1900f2a214c5
mrsaturnino <andre_santos2012@hotmail.com>
2022-07-24 10:01:57 -0300

    Funções em JS

---
+++ aulas/aula16_funcoes/funcoes01.js
@@ -0,0 +1,9 @@
+function parouimpar(n) {
+    if (n%2 == 0) {
+        return `O número ${n} é Par!`
+    } else {
+        return `O número ${n} é Ímpar!`
+    }
+}
+
+console.log(parouimpar(56))
---
+++ aulas/aula16_funcoes/funcoes03.js
@@ -0,0 +1,7 @@
+//funções dentro de variáveis
+
+let v = function(n) {
+    return n*2
+}
+
+console.log(v(3))
