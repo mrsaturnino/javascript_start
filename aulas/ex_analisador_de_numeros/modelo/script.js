bbb983a1d2e2493a1f973adcbc264189c506b47c
André Saturnino <andre_santos2012@hotmail.com>
2022-07-27 17:24:06 -0300

    Analisador de numeros parte 1

--- aulas/ex_analisador_de_numeros/modelo/script.js
+++ aulas/ex_analisador_de_numeros/modelo/script.js
@@ -1,3 +1,29 @@
-function analyzer() {
-    
+//resgatando valores de entrada dos inputs e botão
+let num = document.querySelector('input#textNum')
+let list = document.querySelector('input#textList')
+let result = document.querySelector('div#result')
+let valores = []
+
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true;
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        window.alert('Tudo certo!')
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
 }
