25f878aa3b7488e793d34a5ee7a6dc5af8bdf27d
mrsaturnino <andre_santos2012@hotmail.com>
2022-07-31 19:45:33 -0300

    Aula bônus, objetos em JavaScript

---
+++ aulas/aula17_objetos/objetos.js
@@ -0,0 +1,10 @@
+let amigo = {
+    nome: 'André',
+    idade: '20',
+    peso: '61.5',
+    adicionar(){
+        console.log('Amigo adicionado!')
+    }
+}
+amigo.adicionar()
+console.log(`${amigo.nome} tem ${idade} anos e pesa ${amigo.peso}`)
