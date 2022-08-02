4ebfb03050a7768f669bc56470b9d18835949c70
André Saturnino <andre_santos2012@hotmail.com>
2022-08-02 15:49:04 -0300

    parametros opcionais

---
+++ aulas/guiadoprogramador/aula02_03_let_var_const/aula01_let_var_const.js
@@ -0,0 +1,7 @@
+const pi = 3.14; //Não podemos alterar o valor de uma constante posteriormente.
+
+//A diferença entre var e let é o escopo
+let nome = "André"; //Global, local e em blocos de codigo
+
+var sobrenome = "Saturnino"; //Global e Local
+
---
+++ aulas/guiadoprogramador/aula04_parametros_opcionais/params.js
@@ -0,0 +1,15 @@
+
+//Em uma função, é obrigatório que em seus parâmetros sejam atribuídos algum valor.
+function soma(a,b) {
+    console.log(a + b)
+} //Exemplo: se chamarmos "soma(10)", irá ocorrer um erro na soma, pois foi atribuído um valor a apenas um dos parâmetros.  
+
+
+//Abaixo, logo na declaração dos parâmetros da função, será atribuído um valor padrão apenas ao parâmetro "b", fazendo com que não exista obrigatoriedade para atribuir um valor a ele.
+function sub(a,b = 80) {
+    console.log(a - b)
+} //Basta chamar a função 'sub()' com apenas o valor de "a".
+
+//A função pode ser chamada com novos valores explícitos nos parâmetros ( sub(50, 20) ), sobreescrevendo os valores padrão dos parâmetros.
+
+//Um detalhe importante é que os parâmetros opcionais têm que ser sempre os últimos, exemplo: soma(a, b, c = 20, d = 10)
